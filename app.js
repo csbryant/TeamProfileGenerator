8635366d23f6d93b1872fc0b8d0d4c3a030470d1
Chris Bryant <chris_bryant@mac.com>
2020-09-05 15:07:03 -0700

    Added lib files for basic roles and base questions

--- app.js
+++ app.js
@@ -4,6 +4,92 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employees = [];
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What is your role?",
+            name: "role",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+    ]).then(function(job) {
+        if (job.role === "Manager") {
+            inquirer.prompt([
+                {
+                    message: "What is your name?",
+                    name: "name"
+                },
+                {
+                    message: "What is your ID?",
+                    name: "id"
+                },
+                {
+                    message: "What is your email?",
+                    name: "email"
+                },
+                {
+                message: "What is your office number?",
+                name: "office"
+                }
+            ]).then(function (addMan) {
+                var newManager = new Manager(addMan.name, addMan.email, addMan.id, addMan.office);
+                console.log(newManager);
+                employees.push(addMan);
+            });
+        } else if (job.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    message: "What is your name?",
+                    name: "name"
+                },
+                {
+                    message: "What is your ID?",
+                    name: "id"
+                },
+                {
+                    message: "What is your email?",
+                    name: "email"
+                },
+                {
+                message: "What is your github username?",
+                name: "github"
+                }
+            ]).then(function (addEng) {
+                var newEngineer = new Engineer(addEng.name, addEng.email, addEng.id, addEng.github);
+                console.log(newEngineer);
+                employees.push(addEng);
+            });
+        } else if (job.role === "Intern") {
+            inquirer.prompt([
+                {
+                    message: "What is your name?",
+                    name: "name"
+                },
+                {
+                    message: "What is your ID?",
+                    name: "id"
+                },
+                {
+                    message: "What is your email?",
+                    name: "email"
+                },
+                {
+                message: "Where did you graduate school?",
+                name: "school"
+                }
+            ]).then(function (addInt) {
+                var newIntern = new Intern(addEng.name, addEng.email, addEng.id, addEng.school);
+                console.log(newIntern);
+                employees.push(addInt);
+            });
+        }
+    })
+}
+
+addEmployee();
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
